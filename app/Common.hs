3642114a34b65db89cd3387a50fff8b44edc0105
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2022-06-30 17:05:33 +0300

    Added module Common with common types
    Co-authored-by: Ilnar <4sitescarp@gmail.com>

---
+++ app/Common.hs
@@ -0,0 +1,13 @@
+-- | Common types and values that will be used by other modules.
+module Common where
+
+-- This is the data types for the music representation that is called Melody
+type Octave = Int
+data Pitch = C | Cs | D | Ds | E | F | Fs | G | Gs | A | As | B
+           deriving Show
+data Duration = Whole | Half | Quarter | Eight | Sixteen
+              deriving Show
+data Symbol = Note Octave Pitch Duration | Rest Duration
+            deriving Show
+newtype Composition = Composition [Symbol]
+                    deriving Show
